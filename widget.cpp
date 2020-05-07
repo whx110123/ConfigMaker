85c7365fb51d4b5a2a20b7333d6da7c42aefdfd9
whx110123 <60013551+whx110123@users.noreply.github.com>
2020-05-07 17:25:41 +0800

    Initial commit

---
+++ widget.cpp
@@ -0,0 +1,17 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QStyleFactory>
+
+Widget::Widget(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+	ui->treeWidget->setStyle(QStyleFactory::create("windows"));
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
